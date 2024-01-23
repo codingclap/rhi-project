56dde7aef0341ce5d006268d77a08de9bee6d8e9
Anuj Kumar <codingclap@gmail.com>
2024-01-23 12:38:49 +0530

    RHI- first puch

---
+++ assets/js/data.js
@@ -0,0 +1,9 @@
+const drive_one = {
+    "slide-1":{
+        heading:"main-heading",
+        subheading:"This is subheading",
+        image:"https://buffer.com/cdn-cgi/image/w=1000,fit=contain,q=90,f=auto/library/content/images/size/w1200/2023/10/free-images.jpg"
+
+    }
+
+}
